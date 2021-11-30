ed7f1bc7ee120777d5810999417ee531d536be4b
Patrick Walters <patrick.walters@angel.com>
2021-11-30 13:03:26 -0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	experimental: {
+		urlImports: [
+			"https://framer.com/m/",
+			"https://framerusercontent.com/",
+			"https://fonts.gstatic.com/",
+			"https://ga.jspm.io/",
+			"https://jspm.dev/"
+		]
+	}
+};
